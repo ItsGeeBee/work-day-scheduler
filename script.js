f12f8d47927aa9fb9f65407908e7ca9b12d622fe
ItsGeeBee <gracelbrown@hotmail.com>
2022-06-11 15:47:55 +1000

    saved text shows in console log - doesnt stay in local storage. Contacting BCS

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-
 var time = $("#time");
 var saveBtn = $("#saveBtn");
-var plans = $('input[plans="plans"]');
+var task = $('input[name ="plans"]');
 
-function savePlans(event) {
-    event.preventDefault();
-
-console.log('plans',plans.val());
-$.each(plans,function()  {
-    selected.push($(this).val());
-  });
-
-console.log('plans', selected.join(' '));
-
+function savePlans(e) {
+    e.preventDefault();
+    console.log('plans',task.val());
+       
+        $('#plans').text(task);
+        localStorage.setItem(task,'plans');
 }
 
 
-saveBtn.on('click', savePlans ());
+saveBtn.on('click', savePlans);
+
+// 1 event.preventDefault();
+// $( ".inner" ).append( "<p>Test</p>" );
+
+// $('#example').append("this text was appended");
+
+// 
+
+
+//    // Display the letter
+//    letterBtn.text(letters[i]);
+//    // Attach the letter element
+//    buttonListEl.append(letterBtn);
 
 
 
-
-
-
-// var timeBefore = moment().isBefore();
-// if (timeBefore = true)
-//     $('#time').addclass('past')
+var timeBefore = moment().isBefore();
+if (timeBefore = true)
+    $('#time1').addClass('past');
+    $('#plans').addClass('past');
+ 
