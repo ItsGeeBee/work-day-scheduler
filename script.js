d901e719d1cbe96907bbe04a824f3663e3b9460c
ItsGeeBee <gracelbrown@hotmail.com>
2022-06-11 13:32:53 +1000

    Timer working. Columns in. Stuck on getting 'plan' to save in textarea

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+
+var time = $("#time");
+var saveBtn = $("#saveBtn");
+var plans = $('input[plans="plans"]');
+
+function savePlans(event) {
+    event.preventDefault();
+
+console.log('plans',plans.val());
+$.each(plans,function()  {
+    selected.push($(this).val());
+  });
+
+console.log('plans', selected.join(' '));
+
+}
+
+
+saveBtn.on('click', savePlans ());
+
+
+
+
+
+
+// var timeBefore = moment().isBefore();
+// if (timeBefore = true)
+//     $('#time').addclass('past')
