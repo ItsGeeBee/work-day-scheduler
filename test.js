c283b29281c2b223dd20759f31847978fbd36df1
ItsGeeBee <gracelbrown@hotmail.com>
2022-06-18 08:58:09 +1000

    get from local storagw working, started working on colour timeblocks

--- test.js
+++ test.js
@@ -1,16 +1,48 @@
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
-savebtn = $(".saveBtn");
+$('#currentDay').text(today.format('MMM Do, YYYY'));
 
-var plan = $(this).siblings(".tasks").val();
+var savebtn = $('.saveBtn');
 
-saveBtn.on('click', function() {
-  var time = $(this).siblings(".tasks").text();
-  localStorage.setItem(time, plan);
+var tasks = $('.tasks')
+
+var container = $('.container')
+
+savebtn.on('click', function() {
+
+  var savedTask = $(this).siblings('.tasks').val();
+
+  var time = $(this).parent().attr('id');
+
+  console.log(time);
+
+  localStorage.setItem(time, JSON.stringify(savedTask));
+
 });
+function timeColour() {
+ var hour = moment().hours();
 
-function pageRefresh() {
+  $('.time-block').each(function() {
 
-  $(".tasks").each(function() {
-    return JSON.parse = localStorage.getItem(plan);
-  })};
+   var currentHour = $(this).attr('id');
+
+   if (currentHour > hour) {
+     $(this).addClass("future");
+}   else if (currentHour === hour) {
+      $(this).addClass("present");
+}   else {
+     $(this).addClass("past");
+}
+ })};
+
+$('#time-nine').val(localStorage.getItem('9'));
+$('#time-ten').val(localStorage.getItem('10'));
+$('#time-eleven').val(localStorage.getItem('11'));
+$('#time-twelve').val(localStorage.getItem('12'));
+$('#time-thirteen').val(localStorage.getItem('13'));
+$('#time-fourteen').val(localStorage.getItem('14'));
+$('#time-fifteen').val(localStorage.getItem('15'));
+$('#time-sixteen').val(localStorage.getItem('16'));
+$('#time-seventeen').val(localStorage.getItem('17'));
+
+
+
